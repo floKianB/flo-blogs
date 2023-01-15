6cf85b6cea5bb10700fb7ac873e727fa520718f6
floKianB <kianbaban@yahoo.com>
2023-01-15 10:13:49 -0500

    navigator functions are setted up

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./navigation.css";
-
-import Navigator from "./Navigator.js";
 
 import menue from "../images/menue.png";
 import logo from '../images/Flo.png';
 
 const Navigation = () => {
-    
+    const [rightPosition, setRightPosition] = useState({right: '100vw'})
+    const shiftNavigators = () => {
+        setRightPosition({right: '0vw'})
+        console.log("Open Navigator")
+    }
+
     return (
         <>
-            <nav>
-                <Navigator />
-            </nav>
+            <div className="navigator" style={rightPosition}>
+                <div className="eachNavigatorContainer1"><p className="eachNavigator1">Paint Clone</p></div>
+                <div className="eachNavigatorContainer2"><p className="eachNavigator2">Quote Generator</p></div>
+                <div className="eachNavigatorContainer3"><p className="eachNavigator3">Joke Teller</p></div>
+                <div className="eachNavigatorContainer4"><p className="eachNavigator4">Music Player</p></div>
+                <div className="eachNavigatorContainer5"><p className="eachNavigator5">Drag and Drop</p></div>
+            </div>
             
             <div className='NavigationContainer'>
                 <nav className="navigation">
@@ -22,7 +29,7 @@ const Navigation = () => {
                         <p>Blogs</p>
                         <p>Contact Us</p>
                     </div>
-                    <img src={menue} alt="menue" className='menueIcon'/>
+                    <img src={menue} alt="menue" className='menueIcon' onClick={shiftNavigators}/>
                 </nav>
             </div>   
         </>
